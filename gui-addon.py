f8bc95cd2087b9a1a3a037f6dff7cc6c12a1af65
CorbinHelms <110421710+CorbinHelms@users.noreply.github.com>
2023-03-26 14:16:16 -0500

    Update gui-addon.py

--- gui-addon.py
+++ gui-addon.py
@@ -1,44 +1,73 @@
-import subprocess
 import tkinter as tk
+import threading
+from chat import BluetoothChat
 
-class BluetoothChatGUI:
-    def __init__(self, master):
+class BluetoothChatGUI(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
         self.master = master
+        self.pack()
+        self.create_widgets()
 
-        self.frame = tk.Frame(self.master)
-        self.frame.pack()
+    def create_widgets(self):
+        self.server_button = tk.Button(self, text="Start as server", command=self.start_server)
+        self.server_button.pack(side="left")
 
-        self.text_box = tk.Text(self.frame, width=50, height=20)
-        self.text_box.pack()
+        self.client_frame = tk.Frame(self)
+        self.client_label = tk.Label(self.client_frame, text="Server MAC address:")
+        self.client_label.pack(side="left")
+        self.client_entry = tk.Entry(self.client_frame)
+        self.client_entry.pack(side="left")
+        self.client_button = tk.Button(self.client_frame, text="Start as client", command=self.start_client)
+        self.client_button.pack(side="left")
+        self.client_frame.pack(side="left")
 
-        self.entry_label = tk.Label(self.frame, text="Enter message:")
-        self.entry_label.pack()
+        self.receive_text = tk.Text(self, height=10, width=50)
+        self.receive_text.pack()
+        self.send_frame = tk.Frame(self)
+        self.send_label = tk.Label(self.send_frame, text="Enter message:")
+        self.send_label.pack(side="left")
+        self.send_entry = tk.Entry(self.send_frame)
+        self.send_entry.pack(side="left")
+        self.send_button = tk.Button(self.send_frame, text="Send", command=self.send_message)
+        self.send_button.pack(side="left")
+        self.send_frame.pack()
 
-        self.entry_box = tk.Entry(self.frame)
-        self.entry_box.pack()
+        self.close_button = tk.Button(self, text="Close", command=self.close)
+        self.close_button.pack(side="bottom")
 
-        self.send_button = tk.Button(self.frame, text="Send", command=self.send_message)
-        self.send_button.pack()
+    def start_server(self):
+        self.chat = BluetoothChat()
+        self.chat.start_server()
+        self.receive_thread = threading.Thread(target=self.receive_message)
+        self.receive_thread.start()
 
-        self.receive_messages()
+    def start_client(self):
+        server_mac_address = self.client_entry.get()
+        self.chat = BluetoothChat()
+        self.chat.start_client(server_mac_address)
+        self.receive_thread = threading.Thread(target=self.receive_message)
+        self.receive_thread.start()
 
-    def receive_messages(self):
+    def receive_message(self):
         while True:
-            message = self.master.receive_message()
-            if message:
-                self.text_box.insert(tk.END, message + "\n")
+            data = self.chat.client_sock.recv(1024)
+            if not data:
+                break
+            self.receive_text.insert(tk.END, data.decode("utf-8"))
+
+        self.receive_text.insert(tk.END, "Connection closed")
 
     def send_message(self):
-        message = self.entry_box.get()
-        self.entry_box.delete(0, tk.END)
-        self.master.send_message(message)
+        message = self.send_entry.get()
+        self.chat.client_sock.send(message.encode("utf-8"))
+        self.send_entry.delete(0, tk.END)
+
+    def close(self):
+        self.chat.close()
+        self.master.destroy()
 
 if __name__ == "__main__":
-    chat_process = subprocess.Popen(["python3", "chat.py"], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-
     root = tk.Tk()
-    root.title("Bluetooth Chat")
-
-    gui = BluetoothChatGUI(chat_process)
-
-    root.mainloop()
+    app = BluetoothChatGUI(master=root)
+    app.mainloop()
